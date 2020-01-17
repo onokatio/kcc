97432b5bc2f09d06119931b458744a44d8737442
onokatio <onokatio@gmail.com>
2020-01-18 02:39:22 +0900

    change varable name

--- main.c
+++ main.c
@@ -5,11 +5,9 @@
 #include <sys/mman.h>
 
 int main(int argc, char *argv[]){
-	char source[200];
-	char line[100];
-	char *str;
+	char *source;
 	struct stat sb;
 	fstat(0, &sb);
-	str = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, 0, 0);
-	printf("source: %s",str);
+	source = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, 0, 0);
+	printf("source: %s",source);
 }
