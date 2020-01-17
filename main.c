481e94d38ca86d7a2bc3a739d94388ffb2f543fa
onokatio <onokatio@gmail.com>
2020-01-18 02:40:25 +0900

    add source2symbol

--- main.c
+++ main.c
@@ -4,10 +4,17 @@
 #include <unistd.h>
 #include <sys/mman.h>
 
+void source2symbol(char *source);
+
 int main(int argc, char *argv[]){
 	char *source;
 	struct stat sb;
 	fstat(0, &sb);
 	source = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, 0, 0);
+
+	source2symbol(source);
+}
+
+void source2symbol(char *source){
 	printf("source: %s",source);
 }
