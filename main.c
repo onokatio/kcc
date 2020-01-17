958fedf425f34067b92c4cdfa95acb390f53e50d
onokatio <onokatio@gmail.com>
2020-01-18 02:37:20 +0900

    remove unneeded line

--- main.c
+++ main.c
@@ -7,12 +7,9 @@
 int main(int argc, char *argv[]){
 	char source[200];
 	char line[100];
-	int fd;
 	char *str;
 	struct stat sb;
-	fd = open("./test/add.c", "O_RDONLY");
 	fstat(0, &sb);
 	str = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, 0, 0);
 	printf("source: %s",str);
-	close(fd);
 }
